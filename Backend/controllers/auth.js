0eed8ce5a460527df1087e1f6e53c146b2b48abb
Erubeyy <aecazares@hotmail.com>
2023-04-24 16:53:40 -0600

    Backend JWT implementation

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,28 +1,89 @@
 const {response} = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const {generateJWT} = require('../helpers/jwt')
 
-const createUser = (req, res = response) => {
+const createUser = async(req, res = response) => {
 
-    const {name, email, password} = req.body;
+    const {email, password} = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
+    try{
+        let user = await User.findOne({email});
+
+        if(user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User already exists'
+            })
+        }
+        user = new User(req.body);
+
+        // Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        await user.save();
+
+        // Generate JWT
+        const token = await generateJWT(user.id, user.name);
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        })
+
+    }catch (error){
+        res.status(500).json({
+            ok: false,
+            msg: 'There was a mistake'
+        })
+    }
+
 }
 
-const userLogin = (req, res = response) => {
+const userLogin = async(req, res = response) => {
 
     const { email, password} = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try{
+        const user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User doesnt exists'
+            })
+        }
+
+        // Confirm password
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect Password'
+            })
+        }
+
+        // Generate JWT
+        const token = await generateJWT(user.id, user.name);
+
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        })
+
+    }catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'There was a mistake'
+        })
+    }
 }
 
 const revalidateToken = (req, res = response) => {
