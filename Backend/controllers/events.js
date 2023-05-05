fe179f494aa6cdbf5630eae2f6a7a53574253dda
Erubeyy <aecazares@hotmail.com>
2023-05-05 16:43:00 -0600

    Backend completed

--- Backend/controllers/events.js
+++ Backend/controllers/events.js
@@ -2,7 +2,7 @@ const {response} = require('express');
 const Event = require('../models/Event');
 
 
-
+// Get all the Events
 const getEvents = async(req, res = response) => {
 
     const events = await Event.find().populate('user', 'name');
@@ -13,6 +13,7 @@ const getEvents = async(req, res = response) => {
     })
 }
 
+// Create a new Event
 const createEvent = async(req, res = response) => {
 
     const event = new Event(req.body);
@@ -33,21 +34,82 @@ const createEvent = async(req, res = response) => {
             msg: "There was an error at creating event"
         })
     }
-
 }
 
+// Update an Event
 const updateEvent = async(req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Updating events'
-    })
+
+    const eventID = req.params.id;
+    const uid = req.uid;
+    
+    try{
+        const event = await Event.findById(eventID);
+
+        if(!event){
+            return res.status(404).json({
+                ok: false,
+                msg: "No event found matching given the ID"
+            })
+        }
+
+        if(event.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                msg: "Don't have permission to modify this event"
+            })
+        }
+
+        const newEvent = {...req.body, user: uid }
+
+        const updatedEvent = await Event.findByIdAndUpdate(eventID, newEvent, {new: true});
+
+        res.json({
+            ok: true,
+            event: updatedEvent
+        })
+    }catch (e) {
+        res.status(500).json({
+            ok: false,
+            msg: "An error occurred when updating event"
+        })
+    }
 }
 
+// Delete an Event
 const deleteEvent = async(req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Deleting events'
-    })
+
+    const eventID = req.params.id;
+    const uid = req.uid;
+
+    try{
+        const event = await Event.findById(eventID);
+
+        if(!event){
+            return res.status(404).json({
+                ok: false,
+                msg: "No event found matching given the ID"
+            })
+        }
+
+        if(event.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                msg: "Don't have permission to delete this event"
+            })
+        }
+
+        const deletedEvent = await Event.findByIdAndDelete(eventID, {new: true});
+
+        res.json({
+            ok: true,
+            msg: `Successfully deleted event with ID: ${event.user.toString()}`
+        })
+    }catch (e) {
+        res.status(500).json({
+            ok: false,
+            msg: "An error occurred when deleting event"
+        })
+    }
 }
 
 
